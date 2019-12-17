d795549bbd0d76cfa72d2f7cb127c99936f48a52
SantiagoHerreG <888@holbertonschool.com>
2019-12-17 17:06:38 +0000

    MySQL setup development: creates a database and an User for localhost

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+-- prepares a MySQL server
+-- Root user, creating a new database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- new user in localhost, password predetermined
+DROP USER 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES; 
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- all privileges to new user in the new database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- select privilege on the database performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- flush
+FLUSH PRIVILEGES;
